f5b7e726d4ca4a00669d941695bb2147c87aba1d
Francis Nicole Paulino <nicoleppaulino@gmail.com>
2026-05-13 05:39:29 +0200

    ooo

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,6 +1,15 @@
 let myFirstString = "ciao,mondo";
 
+myFirstString = "hello,world";
+
 console.log(myFirstString);
+
+
+
+let punteggio;
+punteggio = 5;
+
+console.log(punteggio);
 
 
 const userName = document.getElementById('userName');
@@ -12,3 +21,48 @@ sendName.addEventListener("click", function(){
 name = userName.value;
   regard.innerText = `Ciao ${name}`;
 })
+
+
+let eta = 20;
+
+if (eta >= 18) {
+    console.log("Puoi entrare");
+}
+
+
+let eta = 15;
+
+if (eta >= 18) {
+    console.log("Puoi entrare");
+}
+let eta = 15;
+
+
+if (eta >= 18) {
+    console.log("Maggiorenne");
+} else {
+    console.log("Minorenne");
+}
+
+
+for (let i = 0; i < 3; i++) {
+    console.log("Ciao");
+}
+
+
+function saluta(nome) {
+    console.log("Ciao " + nome);
+}
+
+saluta(luca)
+
+
+let frutti = ["mela", "banana", "pera"];
+console.log(frutti[0]);
+console.log(frutti[1]);
+console.log(frutti[2]);
+
+
+document.getElementById("btn").addEventListener("click", function() {
+    console.log("Hai cliccato");
+});
