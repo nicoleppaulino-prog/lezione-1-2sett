c278ca68589a526cecf4f903f44d367cda0281bf
Francis Nicole Paulino <nicoleppaulino@gmail.com>
2026-05-13 04:12:25 +0200

    aaa

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,3 +1,14 @@
-console.log("hello,world");
+let myFirstString = "ciao,mondo";
+
+console.log(myFirstString);
 
 
+const userName = document.getElementById('userName');
+const sendName = document.getElementById('sendName');
+const regard = document.getElementById('regard');
+let name;
+
+sendName.addEventListener("click", function(){
+name = userName.value;
+  regard.innerText = `Ciao ${name}`;
+})
